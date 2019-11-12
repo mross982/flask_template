80ab1a0ad64bd15245ee3a29a138534f4d4f3cc0
Williams <mrwilliams@ascension.org>
2019-11-12 13:31:47 -0600

    no real changes

--- app/models.py
+++ app/models.py
@@ -93,75 +93,53 @@ class Data(db.Model):
 
 '''
 Troubleshooting
-
 interact with app database by starting shell
 (venv) $ flask shell
-
 print list of tables in the database
 >>> print(db.metadata.tables.keys())
-
 >>> for table in db.metadata.tables.keys():
 ...     tb = db.table(table)
 ...     pprint(tb.__dict__.keys())
-
-
 to see the def__repr__ results type the following into the interpreter:
-
 >>> flask shell
-
 >>> from app.models import User
 >>> u = User(username='susan', email='susan@example.com')
 >>> u
 <User susan>
-
 The first time you create a new app, you will need to enter the following into the interpreter:
-
 (venv) $ flask db init
-
 With the migration repository in place, it is time to create the first database migration, 
 which will include the users table that maps to the User database model. There are two ways 
 to create a database migration: manually or automatically. To generate a migration automatically, 
 Alembic compares the database schema as defined by the database models, against the actual 
 database schema currently used in the database. It then populates the migration script with the 
 changes necessary to make the database schema match the application models.
-
 In this case, since there is no previous database, the automatic migration will add the entire 
 User model to the migration script. The flask db migrate sub-command generates these automatic 
 migrations:
-
 (venv) $ flask db migrate -m "users table"
-
 If you were to add new fields to the database, you would need to perform this same step
 (venv) $ flask db migrate -m "new fields in user model"
-
 The generated migration script has two functions called upgrade() and downgrade(). The upgrade() 
 function applies the migration, and the downgrade() function removes it.
-
 The flask db migrate command does not make any changes to the database, it just generates the 
 migration script. To apply the changes to the database, the flask db upgrade command must be used.
-
 (venv) $ flask db upgrade
-
 Because this application uses SQLite, the upgrade command will detect that a database does not exist 
 and will create it (you will notice a file named app.db is added after this command finishes, that 
 is the SQLite database). When working with database servers such as MySQL and PostgreSQL, you have 
 to create the database in the database server before running upgrade.
-
 Note that Flask-SQLAlchemy uses a "snake case" naming convention for database tables by default. For 
 the User model above, the corresponding table in the database will be named user. For a 
 AddressAndPhone model class, the table would be named address_and_phone. If you prefer to choose your 
 own table names, you can add an attribute named __tablename__ to the model class, set to the desired 
 name as a string.
-
 From the interpreter:
-
 >>> from app import db
 >>> from app.models import User, Post
-
 >>> u = User(username='susan', email='susan@example.com')
 >>> db.session.add(u)
 >>> db.session.commit()
-
 >>> users = User.query.all()
 >>> users
 [<User john>, <User susan>]
@@ -170,14 +148,11 @@ From the interpreter:
 ...
 1 john
 2 susan
-
 >>> u = User.query.get(1)
 >>> p = Post(body='my first post!', author=u)
 >>> db.session.add(p)
 >>> db.session.commit()
-
 Additional database queries
-
 >>> # get all posts written by a user
 >>> u = User.query.get(1)
 >>> u
@@ -185,27 +160,22 @@ Additional database queries
 >>> posts = u.posts.all()
 >>> posts
 [<Post my first post!>]
-
 >>> # same, but with a user that has no posts
 >>> u = User.query.get(2)
 >>> u
 <User susan>
 >>> u.posts.all()
 []
-
 >>> # print post author and body for all posts 
 >>> posts = Post.query.all()
 >>> for p in posts:
 ...     print(p.id, p.author.username, p.body)
 ...
 1 john my first post!
-
 # get all users in reverse alphabetical order
 >>> User.query.order_by(User.username.desc()).all()
 [<User susan>, <User john>]
-
 Final Clean up
-
 >>> users = User.query.all()
 >>> for u in users:
 ...     db.session.delete(u)
@@ -216,43 +186,34 @@ Final Clean up
 ...
 >>> db.session.commit()
 ...
-
 ******************************** FOLLOWERS *******************************
 The representation of a many-to-many relationship requires the use of an 
 auxiliary table called an association table.
 While it may not seem obvious at first, the association table with its 
 two foreign keys is able to efficiently answer all the queries about the relationship.
-
 Looking at the summary of all the relationship types, it is easy to determine that 
 the proper data model to track followers is the many-to-many relationship, because 
 a user follows many users, and a user has many followers. 
 But there is a twist. In the students and teachers example I had two entities that 
 were related through the many-to-many relationship. But in the case of followers, I 
 have users following other users, so there is just users.
-
 The second entity of the relationship is also the users. A relationship in which 
 instances of a class are linked to other instances of the same class is called a 
 self-referential relationship.
-
 AFter creating the new relationshio table,
 The changes to the database need to be recorded in a new database migration:
-
 (venv) $ flask db migrate -m "followers"
 (venv) $ flask db upgrade
-
 the is_following() supporting method to make sure the requested action makes sense. 
 For example, if I ask user1 to follow user2, but it turns out that this following 
 relationship already exists in the database, I do not want to add a duplicate. The 
 same logic can be applied to unfollowing.
-
 Obtaining the Posts from Followed Users
-
 The most obvious solution is to run a query that returns the list of followed users, 
 which as you already know, it would be user.followed.all(). Then for each of these 
 returned users I can run a query to get the posts. Once I have all the posts I can 
 merge them into a single list and sort them by date. Sounds good? Well, not really.
 This is actually an awful solution that does not scale well.
-
 There is really no way to avoid this merging and sorting of blog posts, but doing 
 it in the application results in a very inefficient process. This kind of work is 
 what relational databases excel at. The database has indexes that allow it to 
@@ -260,10 +221,8 @@ perform the queries and the sorting in a much more efficient way that I can
 possibly do from my side. So what I really want is to come up with a single 
 database query that defines the information that I want to get, and then let the 
 database figure out how to extract that information in the most efficient way.
-
 Joins
 Post.query.join(followers, (followers.c.followed_id == Post.user_id))
-
 imaging the table structure required to retrieve the posts of of a uers's followed
 users. 
 JOIN
@@ -274,17 +233,10 @@ FILTER
 Forth filter query results for the posts followed by a single user
 ORDER BY
 Fifth sort qurey results by (Post.timestamp.desc()) for example
-
-
-
-
-
-
 Even though adding and removing followers is fairly easy, I want to promote reusability 
 in my code, so I'm not going to sprinkle "appends" and "removes" through the code. Instead,
 I'm going to implement the "follow" and "unfollow" functionality as methods in the User model. 
 It is always best to move the application logic away from view functions and into models or 
 other auxiliary classes or modules, because as you will see later in this chapter, that makes 
 unit testing much easier.
-
 '''
--- migrations/versions/d93cbf42eab5_.py
+++ migrations/versions/d93cbf42eab5_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: b9498fd04b3c
+Revision ID: d93cbf42eab5
 Revises: 
-Create Date: 2019-11-12 11:05:21.585596
+Create Date: 2019-11-12 13:28:51.608388
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'b9498fd04b3c'
+revision = 'd93cbf42eab5'
 down_revision = None
 branch_labels = None
 depends_on = None
